c943870c729a9c20ed0457d8d1a97cb4852c2b05
Leighton Tidwell <leighton.tidwell57@gmail.com>
2022-09-26 09:01:00 -0500

    fixing drop down bug (#35)

--- __tests__/components/userDropDown.test.tsx
+++ __tests__/components/userDropDown.test.tsx
@@ -1,5 +1,5 @@
 import { UserDropdown } from '@/components/index';
-import { userEvent, render, screen, waitFor } from '../test-utils';
+import { userEvent, fireEvent, render, screen, waitFor } from '../test-utils';
 
 describe('UserDropdown', () => {
   describe('when rendered', () => {
@@ -43,35 +43,85 @@ describe('UserDropdown', () => {
       );
     });
 
-    it('should update accessibility when leaving menu', async () => {
+    it('should update accessibility when leaving the button', async () => {
       render(<UserDropdown />);
 
-      await waitFor(() => {
-        userEvent.hover(
-          screen.getByRole('button', { name: 'Profile options' })
-        );
-        userEvent.hover(screen.getByTestId('profile-dropdown'));
-        userEvent.unhover(screen.getByTestId('profile-dropdown'));
-      });
+      await waitFor(
+        () => {
+          userEvent.hover(
+            screen.getByRole('button', { name: 'Profile options' })
+          );
+          userEvent.hover(screen.getByTestId('profile-dropdown'));
+          userEvent.unhover(screen.getByTestId('profile-dropdown'));
+          userEvent.unhover(
+            screen.getByRole('button', { name: 'Profile options' })
+          );
+          return new Promise((r) => setTimeout(r, 1000)); // To account for the timer in the component
+        },
+        { timeout: 8000 }
+      );
 
       expect(
         screen.getByRole('button', { name: 'Profile options' })
       ).toHaveAttribute('aria-expanded', 'false');
+
+      expect(screen.getByTestId('profile-dropdown')).toHaveAttribute(
+        'aria-hidden',
+        'true'
+      );
+    });
+  });
+
+  describe('when tab navigating', () => {
+    it('should show the log out menu and update accessibility', async () => {
+      render(<UserDropdown />);
+
+      await waitFor(() => {
+        fireEvent.focus(
+          screen.getByRole('button', { name: 'Profile options' })
+        );
+      });
+
+      expect(
+        screen.getByRole('button', { name: 'Profile options' })
+      ).toHaveAttribute('aria-expanded', 'true');
+    });
+
+    it('should update accessibility when hovering over menu', async () => {
+      render(<UserDropdown />);
+
+      await waitFor(() => {
+        fireEvent.focus(
+          screen.getByRole('button', { name: 'Profile options' })
+        );
+        fireEvent.focus(screen.getByTestId('profile-dropdown'));
+      });
+
+      expect(screen.getByTestId('profile-dropdown')).toHaveAttribute(
+        'aria-hidden',
+        'false'
+      );
     });
 
     it('should update accessibility when leaving the button', async () => {
       render(<UserDropdown />);
 
-      await waitFor(() => {
-        userEvent.hover(
-          screen.getByRole('button', { name: 'Profile options' })
-        );
-        userEvent.hover(screen.getByTestId('profile-dropdown'));
-        userEvent.unhover(screen.getByTestId('profile-dropdown'));
-        userEvent.unhover(
-          screen.getByRole('button', { name: 'Profile options' })
-        );
-      });
+      await waitFor(
+        () => {
+          fireEvent.focus(
+            screen.getByRole('button', { name: 'Profile options' })
+          );
+          fireEvent.focus(screen.getByTestId('profile-dropdown'));
+          fireEvent.focus(screen.getAllByText('Log out')[0]);
+          fireEvent.blur(screen.getAllByText('Log out')[0]);
+          fireEvent.blur(screen.getByTestId('profile-dropdown'));
+          fireEvent.blur(
+            screen.getByRole('button', { name: 'Profile options' })
+          );
+          return new Promise((r) => setTimeout(r, 1000)); // To account for the timer in the component
+        },
+        { timeout: 8000 }
+      );
 
       expect(
         screen.getByRole('button', { name: 'Profile options' })
